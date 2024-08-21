a9226fc49a8f456ac0331c600ed2266813452dd7
Maria Eduarda Miranda <madumiranda846@gmail.com>
2024-08-21 10:31:07 -0300

    Simulação de 2 carros em aula

--- src/main.py
+++ src/main.py
@@ -23,26 +23,29 @@ def operar_carro(carro : Carro):
     print(carro)
 
 if __name__ == "__main__":
-    print('Cadastre o primeiro carro')
+    print('Cadastre o segundo carro')
     nm_modelo = input(' Digite o modelo: ')
     nm_marca = input(' Digite a marca: ')
     nm_cor = input(' Digite a cor: ')
+    litros = float(input(' Nivel do tanque: '))
+    cm = float(input(' Consumo medio: '))
 
-
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
     print('Cadastre o segundo carro')
     nm_modelo = input(' Digite o modelo: ')
     nm_marca = input(' Digite a marca: ')
     nm_cor = input(' Digite a cor: ')
+    litros= float(input(' Nivel do tanque: '))
+    cm= float(input(' Consumo medio: '))
 
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
 
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro < 600:
+    while carro1.odometro < 600 and carro2.odometro < 600 and (carro1.tanque > 0 or carro2.tanque > 0):
         try:
             op= 0
             while op not in (1,2):
